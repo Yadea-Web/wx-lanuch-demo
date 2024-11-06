4811beef8656bb92cca1bcc9f8954bd23f0abb5e
Yadea <miss_yadi@163.com>
2024-11-06 15:41:15 +0800

    Create shareActivity.vue

---
+++ src/views/shareActivity.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="relative">
+          没有显示「App内打开」的字就是对的
+            <div v-is="'script'">
+              <div style="border: none">
+                APP内打开
+              </div>
+            </div>
+  </div>
+</template>
+<script lang="ts" setup>
+
+</script>
